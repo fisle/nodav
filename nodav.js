f3a65dece38400872e40d19c57aa7b3830a2fbdd
fisle <fisle@fisle.eu>
2015-07-28 20:49:15 +0300

    Add first prototype

---
+++ nodav.js
@@ -0,0 +1,25 @@
+/**
+ * nodav
+ * https://github.com/fisle/nodav
+ *
+ * Copyright (c) 2015 Petri Immonen
+ * Licensed under the MIT license.
+ */
+
+// Require our dependencies first
+var request = require('request');
+var exports = module.exports = {};
+
+/**
+ * Connect function initiates connection to a caldav server
+ * @param array
+ */
+exports.connect = function(config) {
+    console.log(config);
+}
+
+exports.getTasks = function() {
+}
+
+exports.getEvents = function() {
+}
