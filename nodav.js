da7c1ae52b7750d4054b1898f357549181b35800
fisle <fisle@fisle.eu>
2015-07-28 21:22:00 +0300

    We OOP now

--- nodav.js
+++ nodav.js
@@ -8,18 +8,44 @@
 
 // Require our dependencies first
 var request = require('request');
-var exports = module.exports = {};
+var method = Nodav.prototype;
 
-/**
- * Connect function initiates connection to a caldav server
- * @param array
- */
-exports.connect = function(config) {
-    console.log(config);
+function Nodav (config) {
+    this._server = config.server;
+    this._username = config.username;
+    this._password = config.password;
 }
 
-exports.getTasks = function() {
-}
+method.get = function(url, method, payload) {
+    method = method || 'GET';
+    payload = payload || '';
+    console.log(url);
+    console.log(method);
 
-exports.getEvents = function() {
-}
+    var options = {
+        url: url,
+        method: method,
+        multipart: [{ body: payload }],
+        headers: { 'User-Agent': 'nodav caldav library' },
+        auth: {
+            'user': this._username,
+            'pass': this._password,
+            'sendImmediately': false
+        }
+    };
+
+    function callback(error, response, body) {
+        console.log(response.statusCode);
+        console.log(body);
+    }
+    request(options, callback);
+};
+
+method.getTasks = function() {
+    this.get(this._server);
+};
+
+method.getEvents = function() {
+};
+
+module.exports = Nodav;
